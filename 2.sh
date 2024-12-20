0af3a33289e6cea7a1c7dfdb6a5832dc5a844ce5
Daen-cpu <85214502+Daen-cpu@users.noreply.github.com>
2024-12-20 12:29:56 +0300

    Create 2.sh

---
+++ 2.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [ $# -ne 2 ]; then
+  echo "укажите директорию и расширение файлов."
+  exit 1
+fi
+
+directory=$1
+extension=$2
+
+if [ ! -d "$directory" ]; then
+  echo "Директория не найдена."
+  exit 1
+fi
+
+files=$(find "$directory" -type f -name "*.$extension")
+
+if [ -n "$files" ]; then
+  echo "$files"
+else
+  echo "Файлы с расширением .$extension не найдены."
+fi
